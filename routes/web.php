6d1c6a20572adc886668b1134f8199b6b8b2deb2
Tiago Looze <tiago.looze28@gmail.com>
2025-11-05 01:54:59 -0300

    Corrigido conflito de rotas 'app.post' usando Route::match

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\AppController;
@@ -12,20 +14,20 @@ Route::view('/chatbot', 'site.components.chatbot')->name('chatbot');
 
 
 #--------- rotas dinamicas ---------
-Route::get('{pagename}',            [AppController::class,'index'])->name('app.index');
-Route::get('{pagename}/create',     [AppController::class,'create'])->name('app.create');
-Route::post('{pagename}/store',     [AppController::class,'store'])->name('app.store');
+Route::get('{pagename}',[AppController::class,'index'])->name('app.index');
+Route::get('{pagename}/create',[AppController::class,'create'])->name('app.create');
+Route::post('{pagename}/store',[AppController::class,'store'])->name('app.store');
 Route::get('{pagename}/{id}/show/', [AppController::class,'show'])->name('app.show');
-Route::get('{pagename}/{id}/edit',  [AppController::class,'edit'])->name('app.edit');
+Route::get('{pagename}/{id}/edit',[AppController::class,'edit'])->name('app.edit');
 Route::put('{pagename}/{id}/update',[AppController::class,'update'])->name('app.update');
-Route::delete('{pagename}/remove',  [AppController::class,'remove'])->name('app.remove');
-Route::delete('{pagename}/destroy',  [AppController::class,'destroy'])->name('app.destroy');
-Route::delete('{pagename}/restore',  [AppController::class,'restore'])->name('app.restore');
+Route::delete('{pagename}/remove',[AppController::class,'remove'])->name('app.remove');
+Route::delete('{pagename}/destroy',[AppController::class,'destroy'])->name('app.destroy');
+Route::delete('{pagename}/restore',[AppController::class,'restore'])->name('app.restore');
 
 
-//rota de post dinâmico geral, ex de uso route('admin.app.post',['processrobot','upload'])   //vai para o /App/ProcessRobotController::(post|get)_upload
-Route::post('{pagename}/{methodname}/{arg1?}',[AppController::class,'post'])->name('app.post');
-Route::put('{pagename}/{methodname}/{arg1?}',[AppController::class,'post'])->name('app.post');
-Route::delete('{pagename}/{methodname}/{arg1?}',[AppController::class,'post'])->name('app.post');
+// rota de post dinâmico geral: Agrupada em Route::match para evitar conflito de nome.
+// Esta linha substitui as 3 rotas separadas (POST, PUT, DELETE) que tinham o mesmo nome 'app.post'.
+Route::match(['post', 'put', 'delete'], '{pagename}/{methodname}/{arg1?}', [AppController::class, 'post'])->name('app.post');
+
+// Rota GET dinâmica continua separada
 Route::get('{pagename}/{methodname}/{arg1?}',[AppController::class,'get'])->name('app.get');
-
